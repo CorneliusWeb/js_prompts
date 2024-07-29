9511ecec2f4b27e35a73b9633b44a5c0fd42cbde
corneliusweb <cornelius.webdev@gmail.com>
2024-07-29 19:21:25 +0100

    My practice today at JS.info. Cool resources

---
+++ index.js
@@ -0,0 +1,23 @@
+let username = prompt("What is your username");
+let regex = /\b(kareen|chiamaka|lambo)\b/i;
+
+if (username === "Kareen") {
+   let pass = prompt("Enter your password");
+   if (pass === "MINE") {
+      alert("Welcome baby!");
+      let message = prompt("Enter any of your name or nickname");
+      if (message.match(regex)) {
+         alert("Hi babbyyyy🥰🤗🥰");
+      } else {
+         alert("Hi there!")
+      }
+   } else if (pass === null || "") {
+      alert("Cancelled");
+   } else {
+      alert("Wrong password! You be thief?🙄")
+   }
+} else if (username === null || "") {
+   alert("Cancelled");
+} else {
+   alert("I don't know you")
+}
